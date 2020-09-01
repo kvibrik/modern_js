bafaa8fc908d5ee3f98fb0b8aedfcfc42a28c076
kvibrik <kvibr@mail.ru>
2020-09-01 18:24:05 +0500

    получение городов и стран в сторе

--- avia_tickets/src/js/store/locations.js
+++ avia_tickets/src/js/store/locations.js
@@ -6,6 +6,21 @@ class Locations {
     this.countries = null;
     this.cities = null;
   }
+
+  // сразу получаем все города и страны от сервера
+  async init() {
+    const response = await Promise.all([this.api.countries(), this.api.cities()]);
+
+    const [countries, cities] = response;
+    this.countries = countries;
+    this.cities = cities;
+
+    return response;
+  }
+  // получаем все города по коду страны
+  getCitiesByCountryCode(code) {
+    return this.cities.filter((city) => city.country_code === code);
+  }
 }
 
 const locations = new Locations(api);
