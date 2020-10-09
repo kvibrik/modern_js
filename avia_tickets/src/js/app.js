98ae2da3c3303ff6349859594c734d9e4ff89a7f
kvibrik <kvibr@mail.ru>
2020-10-09 16:37:35 +0500

    подключил материалайз

--- avia_tickets/src/js/app.js
+++ avia_tickets/src/js/app.js
@@ -1,7 +1,13 @@
 import '../css/style.css';
+import './plugins';
 import locations from './store/locations';
 
-locations.init().then((res) => {
+document.addEventListener('DOMContentLoaded', () => {
+  const elems = document.querySelector('.sidenav');
+  const instances = M.Sidenav.init(elems);
+});
+
+locations.init().then(res => {
   console.log(res);
   console.log(locations);
   console.log(locations.getCitiesByCountryCode('PE'));
