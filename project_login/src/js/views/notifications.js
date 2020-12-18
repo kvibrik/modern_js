b8539656bc4ebb422fdf00fe387912eb5bcedc78
kvibrik <kvibr@mail.ru>
2020-12-19 00:25:38 +0500

    создание функции notify и создание описания

---
+++ project_login/src/js/views/notifications.js
@@ -0,0 +1,13 @@
+/**
+ *Function notify. Show notfication message
+ * @param {Object} settings
+ * @param {String} settings.msg
+ * @param {String} settings.className
+ * @param {Number} settings.timeout
+ */
+
+export function notify({
+  msg = 'Info message',
+  className = 'alert-info',
+  timeout = 2000,
+} = {}) {}
