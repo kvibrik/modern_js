3e59b5fb62a330b69797376b9460c21e4e36493b
kvibrik <kvibr@mail.ru>
2021-05-12 21:53:03 +0500

    movies_app: delete project

--- movies_app/src/plugins/bootstrap/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import { BootstrapVue } from 'bootstrap-vue';
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-Vue.use(BootstrapVue);
