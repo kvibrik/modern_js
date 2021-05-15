cb82fa9b532dfeec81af2b3d726dc96b14feb6b3
kvibrik <kvibr@mail.ru>
2021-05-15 22:02:06 +0500

    movies_app: change actions and add getters to stor

--- movies_app/src/store/modules/movies.js
+++ movies_app/src/store/modules/movies.js
@@ -1,19 +1,28 @@
+import IDs from '@/store/mock/imdb_top250';
 import axios from '@/plugins/axios';
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+  },
+  getters: {
+    slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
   mutations: {},
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      // tt0111161
-      const response = await axios.get('/', {
-        params: {
-          i: 'tt0111161',
-        },
-      });
+    async fetchMovies({ getters }) {
+      const { slicedIDs, currentPage, moviesPerPage } = getters;
+      const from = currentPage * moviesPerPage - moviesPerPage;
+      const to = currentPage * moviesPerPage;
+      const moviesToFetch = slicedIDs(from, to);
+
+      const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+      const response = await Promise.all(requests);
       console.log(response);
     },
   },
