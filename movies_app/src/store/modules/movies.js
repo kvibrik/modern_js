dc1cea8f11d901f40525f0398c0932d9a4edd1ec
kvibrik <kvibr@mail.ru>
2021-05-16 18:09:08 +0500

    movies_app: add fetchMovies action

--- movies_app/src/store/modules/movies.js
+++ movies_app/src/store/modules/movies.js
@@ -1,5 +1,15 @@
 import IDs from '@/store/mock/imdb_top250';
 import axios from '@/plugins/axios';
+import mutations from '../mutations';
+
+function serializeResponse(movies) {
+  return movies.reduce((acc, movie) => {
+    acc[movie.imdbID] = movie;
+    return acc;
+  }, {});
+}
+
+const { MOVIES } = mutations;
 
 const moviesStore = {
   namespaced: true,
@@ -7,23 +17,38 @@ const moviesStore = {
     top250IDs: IDs,
     moviesPerPage: 12,
     currentPage: 1,
+    movies: {},
   },
   getters: {
     slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
     currentPage: ({ currentPage }) => currentPage,
     moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
   },
-  mutations: {},
+  mutations: {
+    [MOVIES](state, value) {
+      state.movies = value;
+    },
+  },
   actions: {
-    async fetchMovies({ getters }) {
-      const { slicedIDs, currentPage, moviesPerPage } = getters;
-      const from = currentPage * moviesPerPage - moviesPerPage;
-      const to = currentPage * moviesPerPage;
-      const moviesToFetch = slicedIDs(from, to);
+    initMoviesStore: {
+      handler() {},
+      root: true,
+    },
+    async fetchMovies({ getters, commit }) {
+      try {
+        const { slicedIDs, currentPage, moviesPerPage } = getters;
+        const from = currentPage * moviesPerPage - moviesPerPage;
+        const to = currentPage * moviesPerPage;
+        const moviesToFetch = slicedIDs(from, to);
 
-      const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
-      const response = await Promise.all(requests);
-      console.log(response);
+        const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+        const response = await Promise.all(requests);
+        const movies = serializeResponse(response);
+
+        commit(MOVIES, movies);
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
