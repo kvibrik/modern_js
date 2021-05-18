a6a1a8fdc3f7e586d5f142887ac07cb2c5479769
kvibrik <kvibr@mail.ru>
2021-05-18 21:24:08 +0500

    movies_app: add router

--- movies_app/src/main.js
+++ movies_app/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import Router from 'vue-router';
 import App from './App.vue';
 import store from './store';
 import './plugins/bootstrap';
 
 Vue.config.productionTip = false;
+Vue.use(Router);
 
 new Vue({
   store,
