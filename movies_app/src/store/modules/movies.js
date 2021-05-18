3cdd3da34c583e6dcf54fa34261f226470145720
kvibrik <kvibr@mail.ru>
2021-05-18 21:28:12 +0500

    movies_app: add action that change current page

--- movies_app/src/store/modules/movies.js
+++ movies_app/src/store/modules/movies.js
@@ -9,7 +9,7 @@ function serializeResponse(movies) {
   }, {});
 }
 
-const { MOVIES } = mutations;
+const { MOVIES, CURRENT_PAGE } = mutations;
 
 const moviesStore = {
   namespaced: true,
@@ -24,10 +24,14 @@ const moviesStore = {
     slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
     currentPage: ({ currentPage }) => currentPage,
     moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+    moviesLength: ({ top250IDs }) => top250IDs.length,
   },
   mutations: {
     [MOVIES](state, value) {
       state.movies = value;
+    },
+    [CURRENT_PAGE](state, value) {
+      state.currentPage = value;
     },
   },
   actions: {
@@ -52,6 +56,10 @@ const moviesStore = {
       } catch (err) {
         console.log(err);
       }
+    },
+    changeCurrentPage({ commit, dispatch }, page) {
+      commit(CURRENT_PAGE, page);
+      dispatch('fetchMovies');
     },
   },
 };
